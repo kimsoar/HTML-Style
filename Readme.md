7785784c20429da6e3236dfbbd5304427c74882f
kimsoar <kimsoar17@gmail.com>
2023-04-04 21:56:44 +0900

    Responsive Sidebar Menu in HTML CSS & JavaScript | Dark/Light Mode

---
+++ Readme.md
@@ -0,0 +1,13 @@
+Side bar 만들기
+
+YOUTUBE
+https://www.youtube.com/watch?v=bFvfqUMjvsA
+
+CODE
+https://www.codinglabweb.com/2022/01/sidebar-menu-in-html-css-javascript.html
+
+FONT
+https://fonts.google.com/specimen/Poppins?query=poppins
+
+BOXICON
+https://boxicons.com/usage
---
+++ script.js
@@ -0,0 +1,24 @@
+const body = document.querySelector("body");
+const sidebar = document.querySelector(".sidebar");
+const toggle = document.querySelector(".toggle");
+const searchBtn = body.querySelector(".search-box");
+const modeSwitch = body.querySelector(".toggle-switch");
+const modeText = body.querySelector(".mode-text");
+
+toggle.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+});
+
+searchBtn.addEventListener("click", () => {
+  sidebar.classList.remove("close");
+});
+
+modeSwitch.addEventListener("click", () => {
+  body.classList.toggle("dark");
+
+  if (body.classList.contains("dark")) {
+    modeText.innerText = "Light Mode";
+  } else {
+    modeText.innerText = "Dark Mode";
+  }
+});
